0be05e7cddcc1682f79f41d66c337a76a17b4632
Gary Lai <garylai1990@gmail.com>
2018-04-18 20:22:54 -0400

    :construction: (docs) set path for read the doc

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../elitsdk'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
